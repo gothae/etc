6a686e6e8668ac94f5187e2fb5d494978d4c5b4b
gothae <koxogml@naver.com>
2021-07-27 01:30:19 +0900

    cookie 보안

--- nodejs/app_cookie.js
+++ nodejs/app_cookie.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
-app.use(cookieParser());
+// app.use(cookieParser());
+//임의의 값 8NEC@*N98c#@om2@CNAfs 집어넣은것임 ->key값으로 사용
+app.use(cookieParser('8NEC@*N98c#@om2@CNAfs'));
 
 var products = {
     1:{title:'The history of web 1'},
@@ -22,7 +24,7 @@ app.get('/products', function(req,res){
 
 app.get('/cart/:id', function(req,res){
     var id = req.params.id;
-    var cart = req.cookies.cart || {};
+    var cart = req.signedCookies.cart || {};
     
     if (!cart[id]){
         cart[id] = 0;
@@ -30,12 +32,12 @@ app.get('/cart/:id', function(req,res){
     cart[id] = parseInt(cart[id]) + 1;
     //쿠키에 의해서 전달된 값은 문자이므로 int로 변환하여 계산해야한다
 
-    res.cookie('cart',cart);
+    res.cookie('cart',cart, signed:true);
     res.redirect('/cart');
 });
 
 app.get('/cart', function(req,res){
-    var cart = req.cookies.cart;
+    var cart = req.signedCookies.cart;
     if (!cart){
         res.send('Empty!');
     }else{
@@ -52,14 +54,16 @@ app.get('/cart', function(req,res){
 });
 
 app.get('/count', function(req,res){
-    if (req.cookies.count){
-        var count = parseInt(req.cookies.count);
+    // if (req.cookies.count){
+    // cookies 대신 signedCookies를 사용함으로써 암호화
+    if (req.signedCookies.count){
+        var count = parseInt(req.signedCookies.count);
     }else{
         var count = 0;
     }
     //위 식을 var count = parseInt(req.cookies.count) || 0; 으로 표현할 수도 있다.
     count += 1;
-    res.cookie('count', count);
+    res.cookie('count', count, {signed:true});
     res.send('count : '+ count);
 });
 
