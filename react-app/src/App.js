32cc6dcf3b95f15f28cd954115d3555c3d220aa6
gothae <koxogml@naver.com>
2021-08-10 15:42:01 +0900

    component

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,45 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React,{ Component } from 'react';
+import TOC from "./components/TOC.js";
+import Content from './components/Content.js';
+import Subject from './components/Subject.js';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Subject title = "web" sub = "world wide web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText ~~"></Content>
       </div>
     );
   }
