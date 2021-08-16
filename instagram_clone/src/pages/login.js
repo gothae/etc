935210ad120f0833aa9ef5c6de2d5895273683a2
gothae <koxogml@naver.com>
2021-08-16 22:02:22 +0900

    0816

---
+++ instagram_clone/src/pages/login.js
@@ -0,0 +1,17 @@
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+export default function Login(){
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+    
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('')
+    
+    const [error, setError] = useState('');
+    const isInvalid = password === '' || emailAddress === '';
+    return(
+        <p>login page</p>
+    );
+}
