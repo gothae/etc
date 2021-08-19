4480f5b604f277f8c850ea8dbf50cd7ac86f05ba
gothae <koxogml@naver.com>
2021-08-19 22:09:48 +0900

    558

--- instagram_clone/src/components/sidebar/index.js
+++ instagram_clone/src/components/sidebar/index.js
@@ -7,8 +7,6 @@ export default function Sidebar(){
     const {  
         user : {docId, fullName, username, userId, following }
     } = useUser();
-
-    console.log('following',following);
     
     return (
         <div className="p-4">
