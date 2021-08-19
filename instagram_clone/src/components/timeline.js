872ee5c7483463d9098ba428e455e734c219fff9
gothae <koxogml@naver.com>
2021-08-19 22:09:41 +0900

    558

--- instagram_clone/src/components/timeline.js
+++ instagram_clone/src/components/timeline.js
@@ -1,4 +1,10 @@
+
+import Skeleton from "react-loading-skeleton";
+
 export default function Timeline(){
+    // 1.유저들 게시글 가져와야함(hook)
+    // 2.로딩중에는 skeleton 사용
+    // 3.게시글(사진)있으면 render
     return (
         <div className="container col-span-2">
             <p>I am the Timeline</p>
--- instagram_clone/src/services/firebase.js
+++ instagram_clone/src/services/firebase.js
@@ -54,3 +54,30 @@ export async function updateFollowedUserFollowers(
             : FieldValue.arrayUnion(loggedInUserdocId) // 팔로우하니까 추가
     })
 }
+
+export async function getPhotos(userId, following){
+    const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('uesrId', 'in', following)
+    .get();
+
+    const userFollowedPhotos = result.doc.map((photo)=>({
+        ...photo.data(),
+        docId : photo.id
+    }));
+
+    const photosWithUserDetails = await Promise.all(
+        userFollowedPhotos.map(async(photo)=>{
+            let userLikedPhoto = false;
+            if(photo.likes.includes(userId)){
+                userLikedPhoto = true;
+            };
+            const user = await getUserByUserId(photo.userId)
+            const {username} = user[0];
+
+            return {username, ...photo, userLikedPhoto};
+        })
+    )
+    retrun photosWithUserDetails;
+}
