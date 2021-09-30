560d1cb3ae95f34bcb53b634a18b06a33a149331
gothae <koxogml@naver.com>
2021-09-30 21:48:06 +0900

    d

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+function Food(props) {
+  return <h1>I Love Potato</h1>;
+}
 function App(){
-  return <div className="App"/>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" sth={true} pa={['hello',1,2,3,4,true]}/>
+    </div>
+  );
 }
 
 export default App;
--- movie/src/index.js
+++ movie/src/index.js
@@ -5,4 +5,5 @@ import App from './App';
 ReactDOM.render(
   <App />,
   document.getElementById('root')
+  // id가 root인 index.html 엘리먼트에 App 컴포넌트를 렌더링한다
 );
