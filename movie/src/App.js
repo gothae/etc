abd74a9ab4044e3161544856928329a8cd298755
gothae <koxogml@naver.com>
2021-10-06 19:49:43 +0900

    134

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,50 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-function Food({name,picture,rating}) {
-  return (
-    <div>
-      <h2>I Love {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src = {picture} alt={name}/>
-    </div>
-  );
-}
-
-const FoodILike = [
-  {
-    id : 1,
-    name : 'kimchi',
-    rating : 5,
-    image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA5MDhfMjIy%2FMDAxNjMxMDc4Mzk2MzU5.EK79MIWCqtqT5b9zebfFG0JsqeQOVctv5baLXY03LiQg.wn0D_Phns_DgOeYL4IsNXQ7uHNNu8t7XZ9XBu0eG8Scg.JPEG.tuktakmarket%2Fbusanbulkimchi0002.jpg&type=a340'
-  },
-  {
-    id: 2,
-    name : 'Samgyeopsal',
-    rating : 4,
-    image: 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA5MDNfMjk3%2FMDAxNjMwNTk3NDQyMzY1.6-vme9RdAttZU0zGf4flJ4eeh_z5mLZabjzvZByNjc4g.D3DBzg_rrEVyt25XGKqv5TUTLkKo8_CjwoTTqqVvadAg.PNG.2verything%2FDSC03249.png&type=sc960_832'
-  },
-  {
-    id : 3,
-    name : "kimbap",
-    rating : 4.5,
-    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.7-rpW2ynK3Mq89GWd2Q36QHaE8%26pid%3DApi%26h%3D160&f=1'
+class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('hello');
   }
-];
-
-function App(){
-  return (
-    <div>
-      {FoodILike.map(item => (
-        <Food key={item.id} name={item.name} picture={item.image} rating={item.rating} />
-      ))};
-    </div>
-  );
+  componentDidMount(){
+    console.log('didmount');
+  }
+  componentDidUpdate(){
+    console.log('didupdate')
+  }
+  componentWillUnmount(){
+    console.log('unmount')
+  }
+  state = {
+    count : 0
+  }
+  add = () => (this.setState({ count: this.state.count + 1 }))
+  minus = () => (this.setState({ count: this.state.count - 1 }))
+  render() {
+    console.log('render');
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>ADD</button>
+        <button onClick={this.minus}>MINUS</button>
+      </div>
+    );
+  }
 }
 
-Food.propTypes = {
-  name : PropTypes.string.isRequired,
-  picture : PropTypes.string.isRequired,
-  rating : PropTypes.number.isRequired,
-}
 export default App;
