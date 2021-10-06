6d28fbca89e441e96d1f8ff43b72ccef0af715cc
gothae <koxogml@naver.com>
2021-10-06 19:58:31 +0900

    136

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,31 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-  componentDidMount(){
-    console.log('didmount');
-  }
-  componentDidUpdate(){
-    console.log('didupdate')
-  }
-  componentWillUnmount(){
-    console.log('unmount')
-  }
   state = {
-    count : 0
-  }
-  add = () => (this.setState({ count: this.state.count + 1 }))
-  minus = () => (this.setState({ count: this.state.count - 1 }))
+    isLoading : true,
+  };
   render() {
-    console.log('render');
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>ADD</button>
-        <button onClick={this.minus}>MINUS</button>
+        <p1>{isLoading ?'Loading' : 'We Are Ready'}</p1>
       </div>
     );
   }
