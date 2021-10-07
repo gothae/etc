76d0c3c9a378c61862654e68e2dba7ea42e372f4
gothae <koxogml@naver.com>
2021-10-08 01:19:47 +0900

    1008

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
-import Home from './routes/Home';
+import React from 'react';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 
 function App(){
-  return <Home/>
+  return (
+    <HashRouter>
+      <Navigation/>
+      <Route path="/home" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail}a/>
+    </HashRouter>
+  );
 }
 
 export default App;
