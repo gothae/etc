eb501b009fcae986557b0cc200175eda0e10fbb1
gothae <koxogml@naver.com>
2021-10-07 16:35:52 +0900

    176

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,14 +1,40 @@
+import axios from 'axios';
 import React, { Component } from 'react';
-
+import Movie from './Movie';
 class App extends Component {
   state = {
     isLoading : true,
+    movies : [],
   };
+  getMovies = async() => {
+    const {
+      data : {
+        data : { movies }
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({ movies, isLoading : false })
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        <p1>{isLoading ?'Loading' : 'We Are Ready'}</p1>
+        {isLoading 
+        ? 'Loading' 
+        : movies.map((movie) => {
+          return (
+            <Movie
+              key = {movie.id}
+              id = {movie.id}
+              year = {movie.year}
+              title = {movie.title}
+              summary = {movie.summary}
+              poster = {movie.poster}
+              />
+            );
+          })}
       </div>
     );
   }
